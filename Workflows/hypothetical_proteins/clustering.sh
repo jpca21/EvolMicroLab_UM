b0f83d13ee0f520a5010b309e3860bbeaf156399
matrs <jlmaturanag@gmail.com>
2021-12-02 13:57:20 -0300

    Add script for clustering hypothetical proteins with mmseqs

---
+++ Workflows/hypothetical_proteins/clustering.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+## Cluster hypothetical proteins to avoid running long searches with hmmer and others ##
+## This reduced the amount of sequences about 10 times ##
+
+root="root/for/this/project/Clustering"
+db_dir="${root}/mmseq_akk_hypo_db"
+fasta="your/multifasta/hypothetical_proteins.fa"
+db_name="akk_hypo"
+
+cd $root
+
+mkdir $db_dir
+mmseqs createdb $fasta $db_dir/$db_name
+
+clusters_dir="mmseqs_cluster90_70"
+clusters_name="akk_hypo_clu"
+
+# Clustering by 90% mutual coverage, 70% identity
+mkdir $clusters_dir
+mmseqs cluster $db_dir/$db_name $clusters_dir/$clusters_name tmp --cov-mode 0 -c 0.9 \
+ --min-seq-id 0.7 --threads 8
+ 
+# Create a "sub-db" and get the representative sequences in fasta format
+
+sub_clusters_dir="mmseqs_cluster90_70_sub"
+sub_clusters_name="akk_hypo_clu_rep"
+
+mkdir $sub_clusters_dir
+
+mmseqs createsubdb $clusters_dir/$clusters_name $db_dir/$db_name \
+ $sub_clusters_dir/$sub_clusters_name
+
+# Representative sequences
+rep_fasta="akk_hyp_clu90_70_rep.fasta"
+ 
+mmseqs convert2fasta $sub_clusters_dir/$sub_clusters_name $rep_fasta
+
+echo **How many sequences are in the original fasta and in the representatives fasta?**
+
+rg -c '^>' $fasta
+rg -c '^>' $rep_fasta
+
